7b44a111c16286eb8abed7b307027b5bcbf61721
diaket13 <k9825@foxmail.com>
2018-05-15 00:36:47 +0800

    剩余价值背包

---
+++ dynamic_programming/src/lintcode/surplus_value_backpack.java
@@ -0,0 +1,21 @@
+package lintcode;
+
+/**
+ * 971号题
+ * https://www.lintcode.com/problem/surplus-value-backpack/description
+ */
+public class surplus_value_backpack {
+    /**
+     * @param k1: The coefficient of A
+     * @param k2: The  coefficient of B
+     * @param c: The volume of backpack
+     * @param n: The amount of A
+     * @param m: The amount of B
+     * @param a: The volume of A
+     * @param b: The volume of B
+     * @return: Return the max value you can get
+     */
+    public long getMaxValue(int k1, int k2, int c, int n, int m, int[] a, int[] b) {
+        // Write your code here
+    }
+}
