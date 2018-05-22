eba315ab9ecee54a363288a3591ee22f457bfbf4
diaket13 <k9825@foxmail.com>
2018-05-23 00:27:54 +0800

    2根指针找无重复最长子串,CA但是没别人快 O(n)

--- two_pointer/src/lintcode/longest_substring_without_repeating_characters.java
+++ two_pointer/src/lintcode/longest_substring_without_repeating_characters.java
@@ -14,27 +14,46 @@ public class longest_substring_without_repeating_characters {
      * @return: an integer
      */
     public int lengthOfLongestSubstring(String s) {
-        //两根指针,一根指向开头,一根往后遍历
+        //两根指针,一根指向开始,一根往后遍历
         int begin=0,end;
         //再来个目前找到的最长的子串的长度
         int length = 0;
         //最后来个hash表来保存已经出现过的字符
-        HashMap<Character,Boolean> map = new HashMap<>();
+        HashMap<Character,Boolean> map = new HashMap<>(26);
         //先获取字符串的char数组
         char[] chars = s.toCharArray();
         if(chars.length==1){
             return 1;
         }
+        /*
+        子字符串位置从begin到end
+        具体思路,end从0开始,找到重复的字符就将此字符第一次出现的位置+1标记为子字符串的开始位置,即begin.
+        保证begin到end之间的所有字符都是不重复的
+         */
+        //开始从头找
         for(end = 0;end<chars.length;end++){
             char c = chars[end];
             Boolean exist = map.get(c);
+            //每遍历一个,就从哈希表中查看是否出现过
             if(exist!=null && exist){
+                //出现了,就先计算当前字符串的长度,将较长的存入length
                 length = length < end-begin ? end-begin : length;
-                for(;begin)
+                //再把从开始位置到出现的字符的位置之前的所有字符都设置为未出现
+                for(;begin<end;begin++){
+                    char b = chars[begin];
+                    if(b == c){
+                        //并将开始位置设置为出现的字符的位置+1
+                        begin++;
+                        break;
+                    }
+                    map.put(b,false);
+                }
+            }else{
+                //没出现,则记为出现
+                map.put(c,true);
             }
-
         }
-        // write your code here
+        return length < end-begin ? end-begin : length;
     }
 
 }
