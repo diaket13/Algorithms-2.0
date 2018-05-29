9a2182204ea5e05c6686c96cebfc3aa13f05292b
yuxiwang1 <yuxiwang1@creditease.cn>
2018-05-29 18:58:44 +0800

    提交

--- divide_and_conquer/src/lintcode/median_of_two_sorted_arrays.java
+++ divide_and_conquer/src/lintcode/median_of_two_sorted_arrays.java
@@ -14,19 +14,81 @@ public class median_of_two_sorted_arrays {
     */
     public double findMedianSortedArrays(int[] A, int[] B) {
         int num = A.length + B.length;
-        int halfNum = num / 2 + 1;
+        int halfNum = num / 2;
         int position;
-        if(A.length > B.length){
-            position = B.length / 2 + 1;
-            findMedian(halfNum,position,B,A);
-        }else {
-            position = A.length / 2 + 1;
-            findMedian(halfNum,position,A,B);
+        if((num & 1)==1){
+            if(A.length > B.length){
+                position = (-1 + B.length) / 2 ;
+                findMedian(halfNum,position,-1,B.length,B,A);
+            }else {
+                position = (-1 + A.length) / 2 ;
+                findMedian(halfNum,position,-1,A.length,A,B);
+            }
+        }else{
+            if(A.length > B.length){
+                position = (-1 + B.length) / 2 ;
+                findTwoMedian(halfNum,position,-1,B.length,B,A);
+            }else {
+                position = (-1 + A.length) / 2 ;
+                findTwoMedian(halfNum,position,-1,A.length,A,B);
+            }
         }
     }
 
-    double findMedian(int halfNum,int position,int[] divide,int[] other){
+    double findMedian(int halfNum,int position,int left,int right,int[] divide,int[] other){
         int aNum = divide[position];
+        //position左边共有 position个元素 还差 halfNum - position个
         int bNum = other[halfNum-position];
+        //aNum 和 bNum 左边共有halfNum个元素
+        int bNum_left = other[halfNum-position-1];
+        //aNum在这两个元素中间的话,说明小于或等于aNum的元素有halfNum个,则aNum为中位数
+        if(aNum >= bNum_left && aNum <=bNum){
+            return aNum+0.0;
+        }
+        if(aNum < bNum_left){
+            if(position == right - 1){
+                return bNum_left+0.0;
+            }
+            int nextPosition = (position + right) / 2;
+            return findMedian(halfNum,nextPosition,position,right,divide,other);
+        }
+        if(aNum > bNum){
+            if(position == left + 1){
+                return bNum+0.0;
+            }
+            int nextPosition = (left + position) / 2;
+            return findMedian(halfNum,nextPosition,left,position,divide,other);
+        }
+    }
+    double findTwoMedian(int halfNum,int position,int left,int right,int[] divide,int[] other){
+        int aNum = divide[position];
+        //position左边共有 position个元素 还差 halfNum - position个
+        int bNum = other[halfNum-position];
+        //aNum 和 bNum 左边共有halfNum个元素
+        int bNum_left = other[halfNum-position-1];
+        //aNum在这两个元素中间的话,说明小于或等于aNum的元素有halfNum个,即aNum为2个中位数的右边那个
+        if(aNum >= bNum_left && aNum <=bNum){
+            int aNum_left = divide[position-1];
+            if(aNum_left < bNum_left){
+                return (aNum + bNum_left)/2.0;
+            }else {
+                return (aNum + aNum_left)/2.0;
+            }
+        }
+        if(aNum < bNum_left){
+            //right代表上一次查的结果 中位数应该在right左边
+            if(position == right - 1){
+                return bNum_left+0.0;
+            }
+            int nextPosition = (position + right) / 2;
+            return findMedian(halfNum,nextPosition,position,right,divide,other);
+        }
+        if(aNum > bNum){
+            if(position == left + 1){
+                return bNum+0.0;
+            }
+            int nextPosition = (left + position) / 2;
+            return findMedian(halfNum,nextPosition,left,position,divide,other);
+        }
     }
 }
